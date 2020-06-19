a174a5daff757405d8fc860d7bc5a0232c844b4e
Usama Ahmed <usamaahmed97@gmail.com>
2020-06-20 04:51:37 +0500

    Project created using create react app

---
+++ src/Pages/HomePage.Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Pages/HomePage.Styles.scss';
+import Directory from '../Components/Directory/Directory-Menu.Component.jsx';
+
+const HomePage = () =>
+{
+    return (<div className='homepage'>
+
+<Directory />
+    </div>)
+}
+
+export default HomePage;
